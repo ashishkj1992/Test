6c9227570e58893f8c074bf19e722988255ab3c4
ashish kumar <tech.ashishkj@gmail.com>
2018-09-09 20:26:25 +0530

    class B implementaed

--- src/com/hibernate/main/LoadCustomer.java
+++ src/com/hibernate/main/LoadCustomer.java
@@ -9,7 +9,8 @@ import com.hibernate.TO.CustomerTo;
 public class LoadCustomer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+;		// TODO Auto-generated method stub
 
 		CustomerDAO cdao = DAOFactory.getCustomerDAO();
 
